ed942b472737cafb32afbdd141852b6f1affd2c7
joshuasamples2 <joshua.samples2@gmail.com>
2022-02-05 12:42:07 -0600

    signup page

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,7 +1,76 @@
+import React,{useState} from "react";
+import { useMutation } from '@apollo/client';
 import { Container } from "react-bootstrap";
 
-export default function Signup() {
+
+export default function Signup(props){
+    const [formState, setFormState] = useState({email: '', password: ''});
+    const [addUser] = useMutation(ADD_USER);
+    const handleFormSubmit = async (event)=>{
+        event.preventDefault();
+        const mutationResponse = await addUser({
+            varibles:{
+                email: formState.email,
+                password: formState.password,
+                firstName: formState.firstName,
+                lastName: formState.lastName,
+            },
+        });
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+    };
+    const handleChange =(event)=>{
+        const {name, value} = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+            });
+    };
     return (
-        <Container>Sign Up Page</Container>
-    )
+<Container><div className='container my-1'>
+    <h2>Signup</h2>
+    <form onSubmit={handleFormSubmit}>
+        <div className='flex-row space-between my-2'>
+            <label htmlFor='firstName'>First Name:</label>
+            <input 
+            placeholder='First'
+            name='firstName'
+            type='firstName'
+            id='firstName'
+            onChange={handleChange}/>
+        </div>
+        <div className='flex-row space-between my-2'>
+        <label htmlFor='lastName'>Last Name:</label>
+            <input 
+            placeholder='Last'
+            name='lastName'
+            type='lastName'
+            id='lastName'
+            onChange={handleChange}/>
+        </div>
+        <div className='flex-row space-between my-2'>
+        <label htmlFor='email'>Email:</label>
+            <input 
+            placeholder='email@test.com'
+            name='email'
+            type='email'
+            id='email'
+            onChange={handleChange}/>
+        </div>
+        <div className='flex-row space-between my-2'>
+        <label htmlFor='pwd'>Password:</label>
+            <input 
+            placeholder='******'
+            name='password'
+            type='password'
+            id='pwd'
+            onChange={handleChange}/>
+        </div>
+        <div className="flex-row flex-end">
+            <button type='submit'>Submit</button>
+        </div>
+    </form>
+</div>
+</Container>
+    );
 }
