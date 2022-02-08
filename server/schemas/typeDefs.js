83323b0e8314690035a65413c44788355bc67384
rob49157 <robertopaipo@hotmail.com>
2022-02-07 19:19:03 -0500

    mutation error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,45 +3,44 @@ const { gql }=require('apollo-server-express')
 const typeDefs=gql`
 
 type Doctor{
-    _id:ID
-    name:String
-    practice:String
-    yearsofexperience:Number
+    _id: ID
+    name: String
+    practice: String
+    yearsofexperience: Int
     location: String
-    thoughts:[Thought]
+    thoughts: [Thought]
 }
-
 type User{
     _id:ID
-    firstname:String
+    firstname: String
     lastname: String
-    email:String
+    email: String
     password: String
 }
 
 type Thought{
-    _id:ID
-    thoughttext:String
-    createat:Date
-    username:User
+    _id: ID
+    thoughttext: String
+    createat: String
+    username: User
 }
-
 type Auth{
-    token:ID
+    token: ID
     user: User
 }
 
-type query{
+type Query{
     doctor:[Doctor]
-     doctors:(_id:ID!, practice:String):Doctor
+    doctors(_id:ID!, practice:String):Doctor
 }
 
-type mutation{
+type Mutation{
     adduser(firstname:String,lastname:String,email:String,password:String):Auth
     updateUser(_id:ID,firstName: String, lastName: String, email: String, password: String): User
     addthought(thoughttext:String!,username:String!):Doctor
     removethought(_id:ID!,thoughttext:String):Doctor
     login(email: String!, password: String!): Auth
-}`
+}
+`
 
 module.exports=typeDefs
