289fe54e5209905c94a52888a8d1dd6e31fa13d4
joshuasamples2 <joshua.samples2@gmail.com>
2022-02-15 20:17:57 -0600

    styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import { Card, Container } from "react-bootstrap";
 export default function Footer() {
     return(
             <Card.Footer>
-            <h1>Contact Us:</h1><a href = "mailto: mail@example.com">Send Email</a>
+            {/* <h1>Contact Us:</h1><a href = "mailto: mail@example.com">Send Email</a> */}
             </Card.Footer>
             
     )
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -48,7 +48,7 @@ const Home = () => {
 
   return (
     <Container>
-      <h1>Search for Doctors in the Atlanta Metro area</h1>
+      <h1>Search for Doctors in the Metro Atlanta area</h1>
       <h2>Find the perfect Doctor for you and your loved ones. 
       </h2>
       <p> For forever now people have relied on their friends to give them a good recommendation for a doctor. Sometimes your friends haven't seen a specific doctor that you need to see. Sometimes it may be embarrising to tell someone other than a doctor what is wrong. Whenever that is the case it is pretty much a shot in the dark when choosing a doctor. Instead of taking taking shots in the dark,  use this application to find a good doctor.</p>
